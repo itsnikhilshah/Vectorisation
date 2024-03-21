99664a53ef51f1f1e31a289c4abccc6efa5de3b6
itsnikhilshah <nikhil.m.shah.1999@gmail.com>
2024-03-21 18:55:59 -0400

    Role Similarity Code

--- vectorising.py
+++ vectorising.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from time import time
 import pickle
 import configparser
+import os
+import pandas as pd
+import glob
 
 
 def clean_course_desc(file_path):
@@ -76,7 +79,7 @@ class PineconeInteraction:
         chunk_size = 200
 
         #storing the calculated values into pickle file to reduce need to constantly process
-        with open('course_data.pkl','wb') as f:
+        with open('data\\course_data.pkl','wb') as f:
             pickle.dump((course_id,course_descriptions),f)
 
 
@@ -88,6 +91,51 @@ class PineconeInteraction:
 
 
             print("Chunk "+str(i)+" has been upserted")
+
+    def embedding_query_with_score(self):
+        folderpath="data\\roles"
+        if not os.path.exists("data\\excel"):
+            os.mkdir('data\\excel')
+        with open('data\\course_data.pkl', 'rb') as f:
+            course_id_list, course_descriptions = pickle.load(f) # loading pickle file of the pre processed course id and course description
+        excel_data_df = pd.read_excel('data\\RefSheet.xlsx', sheet_name='Sheet1',usecols=['Name','Course ID'],index_col=0)
+        course_id_to_name =  excel_data_df.iloc[:, 0].to_dict()
+        course_id_to_name = {v: k for k, v in course_id_to_name.items()}
+        self.corpus_sentences=course_descriptions
+        for filename in glob.glob(os.path.join(folderpath,'*.txt')):
+            filename_sheet=filename
+            query_sentences=[] 
+            txt_file = open(filename,"r", encoding='utf8') #loading query file
+            for line in txt_file:
+                query_sentences.append(line.strip()) #storing query sequentially in a list
+            query_embeddings = self.model.encode(query_sentences).tolist() #vectorising the queries using roberta large
+            df = pd.DataFrame(columns=['KSAT', 'Course 1','Course Name 1','Score 1', 'Course 2','Course Name 2','Score 2', 'Course 3','Course Name 3','Score 3', 'Course 4','Course Name 4','Score 4','Course 5','Course Name 5','Score 5'])
+
+            for query_embedding, query_sentence in zip(query_embeddings, query_sentences):
+
+                #res = self.index.query(vector=query_embedding, namespace="course_descriptions", top_k=5, include_values=True)
+                res = self.index.query(vector=query_embedding, namespace="test", top_k=5, include_values=True)
+                # Extract the course IDs from the top 5 matches
+                course_ids = [res_match.id for res_match in res.matches]
+                scores = [res_match.score for res_match in res.matches]
+                course_names = [course_id_to_name[course_id] if course_id in course_id_to_name else "Unknown" for course_id in course_ids]
+
+
+                # If fewer than 5 matches, fill the remaining slots with NaN
+                while len(course_ids) < 5:
+                    course_ids.append(pd.np.nan)
+                    course_names.append(pd.np.nan)
+                    scores.append(pd.np.nan)
+                # Add a new row to the DataFrame
+                df.loc[len(df)] = [query_sentence] + [val for sublist in zip(course_ids, course_names, scores) for val in sublist]
+
+            
+            base = os.path.basename(filename_sheet)
+            name, ext = os.path.splitext(base)
+            df.to_excel('data\\excel\\'+name+'.xlsx',sheet_name=name, index=False)
+            df=df.iloc[0:0]
+            print(name+" done")
+
         
     def delete_namespace(self, namespace):
         self.index.delete(deleteAll='true', namespace=namespace)
